002f468be3ecdb13a694d056ce9649a1fae1ce1d
pavakta <pavakta@myseneca.ca>
2019-09-24 13:32:20 -0400

    Updates

---
+++ precache-manifest.32545739fb6c40b5ef77642aa40d6a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ffe18af57284c077af5fc6cc18dd88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f5558d33a8e4c09fe1",
+    "url": "/static/css/main.24630403.chunk.css"
+  },
+  {
+    "revision": "fb1d90e77751e94dc7da",
+    "url": "/static/js/2.cc095dc4.chunk.js"
+  },
+  {
+    "revision": "00f5558d33a8e4c09fe1",
+    "url": "/static/js/main.b01fb12e.chunk.js"
+  },
+  {
+    "revision": "226d8216db6d0ea6e981",
+    "url": "/static/js/runtime~main.f40cd5bb.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/buildyourburger/precache-manifest.15a9387186af899594ae934ca317cc1f.js"
+  "/precache-manifest.32545739fb6c40b5ef77642aa40d6a36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/buildyourburger/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
